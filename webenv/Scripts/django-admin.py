fcb4650024988e68a069df11e3ff2bf31dac92f1
atif <atifashhar@ggmail.com>
2020-04-15 20:57:18 +0530

    first commit

---
+++ webenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!e:\project_work\web development\first_project\webenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
